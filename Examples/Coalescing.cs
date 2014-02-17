7d09a40d5dec57fa94f68c909de08e3198477774
david <david@federnet.com>
2014-02-17 13:40:50 -0500

    Coalesce Operator

---
+++ Examples/Coalescing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    class Coalescing
+    {
+        public Coalescing()
+        {
+            object evaluator = null;
+
+            // Shorthand coalsce operator
+            object result = evaluator ?? new object();
+
+            // Expands to //
+            result = evaluator == null ? new object() : evaluator;
+            
+            // Expands to //
+            if (evaluator == null)
+                result = new object();
+            else
+                result = evaluator;
+
+        }
+    }
+}
